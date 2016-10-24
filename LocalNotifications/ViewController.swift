df6e831730f579a76206341e7984f0880df84b2b
Bart Jacobs <developer@bartjacobs.com>
2016-10-24 06:22:38 +0200

    Define notification actions and category

--- LocalNotifications/ViewController.swift
+++ LocalNotifications/ViewController.swift
@@ -11,13 +11,26 @@ import UserNotifications
 
 class ViewController: UIViewController {
 
+    struct Notification {
+
+        struct Category {
+            static let tutorial = "tutorial"
+        }
+
+        struct Action {
+            static let readLater = "readLater"
+            static let showDetails = "showDetails"
+            static let unsubscribe = "unsubscribe"
+        }
+
+    }
+
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Configure User Notification Center
-        UNUserNotificationCenter.current().delegate = self
+        configureUserNotificationsCenter()
     }
 
     // MARK: - Actions
@@ -44,6 +57,22 @@ class ViewController: UIViewController {
 
     // MARK: - Private Methods
 
+    private func configureUserNotificationsCenter() {
+        // Configure User Notification Center
+        UNUserNotificationCenter.current().delegate = self
+
+        // Define Actions
+        let actionReadLater = UNNotificationAction(identifier: Notification.Action.readLater, title: "Read Later", options: [])
+        let actionShowDetails = UNNotificationAction(identifier: Notification.Action.showDetails, title: "Show Details", options: [.foreground])
+        let actionUnsubscribe = UNNotificationAction(identifier: Notification.Action.unsubscribe, title: "Unsubscribe", options: [.destructive, .authenticationRequired])
+
+        // Define Category
+        let tutorialCategory = UNNotificationCategory(identifier: Notification.Category.tutorial, actions: [actionReadLater, actionShowDetails, actionUnsubscribe], intentIdentifiers: [], options: [])
+
+        // Register Category
+        UNUserNotificationCenter.current().setNotificationCategories([tutorialCategory])
+    }
+
     private func requestAuthorization(completionHandler: @escaping (_ success: Bool) -> ()) {
         // Request Authorization
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
@@ -63,6 +92,9 @@ class ViewController: UIViewController {
         notificationContent.title = "Cocoacasts"
         notificationContent.subtitle = "Local Notifications"
         notificationContent.body = "In this tutorial, you learn how to schedule local notifications with the User Notifications framework."
+
+        // Set Category Identifier
+        notificationContent.categoryIdentifier = Notification.Category.tutorial
 
         // Add Trigger
         let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
@@ -86,4 +118,16 @@ extension ViewController: UNUserNotificationCenterDelegate {
         completionHandler([.alert])
     }
     
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier {
+        case Notification.Action.readLater:
+            print("Save Tutorial For Later")
+        case Notification.Action.unsubscribe:
+            print("Unsubscribe Reader")
+        default:
+            print("Other Action")
+        }
+
+        completionHandler()
+    }
 }
